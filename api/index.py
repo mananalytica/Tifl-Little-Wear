218b22ae77ec8d94b37a361908ec39e2928589e2
Abdul Manan <ab@Abduls-MacBook-Air.local>
2026-07-24 12:04:00 +0500

    Fix rudderstack package name and import path

--- api/index.py
+++ api/index.py
@@ -35,7 +35,7 @@ from datetime import datetime, timedelta
 os.environ["HOME"] = "/tmp"
 
 import duckdb
-from rudderstack import analytics as rudder_analytics
+import rudderstack.analytics as rudder_analytics
 from fastapi import FastAPI, HTTPException, Header
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi.responses import Response
